c1b78b0f53e2d69c4c1425de0563c0225e4ed7d8
Jodok Vieli <jvieli@student.ethz.ch>
2020-12-27 13:58:30 +0100

    sketch model

---
+++ model.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+)
+
+// TODO: Implement all pieces
+
+// TODO: Model piece
+
+// TODO: Model direction
+
+// TODO: Model game
+
+/* TODO: Methods for game
+
+deletePiece()
+
+placePiece()
+
+pieceFits()
+
+movePiece(direction=[down, left, right])
+
+rotate()
+
+checkRow()
+
+fillBoard()
+
+spawnPiece()
+
+
+
