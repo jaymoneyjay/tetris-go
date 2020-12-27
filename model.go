1d0d29069fabe1873854bb81485c2a6b22e17f38
Jodok Vieli <jvieli@student.ethz.ch>
2020-12-27 14:42:50 +0100

    model game, piece, and direction

--- model.go
+++ model.go
@@ -1,33 +1,113 @@
 package main
 
 import (
+	"time"
 )
 
-// TODO: Implement all pieces
+// Constants
+const boardHeight = 10
+const boardWidth = 20
+const gameSpeed = 1000 * time.Millisecond
 
-// TODO: Model piece
+// Collection of all possible tetris pieces modeled with offset coordinates
+pieces = []Pieces{
+	//half cross
+	newPiece({0, 1, 2, 1}, {0, 0, 0, 1}),
+	//straight
+	newPiece({0, 1, 2, 3}, {0, 0, 0, 0}),
+	//left L
+	newPiece({0, 1, 1, 1}, {0, 0, 1, 2}),
+	//right L
+	newPiece({0, 0, 0, 1}, {0, 1, 2, 1}),
+	//square
+	newPiece({0, 0, 1, 1}, {0, 1, 0, 1}),
+	//left knee
+	newPiece({0, 0, 1, 1}, {0, 1, 1, 2}),
+	//right knee
+	newPiece({0, 0, 1, 1}, {0, 1, 0, -1})
+}
 
-// TODO: Model direction
+// Struct to model the tetris piece
+type Piece struct {
+	deltaX []int
+	deltaY []int
+}
 
-// TODO: Model game
+// Create a new piece, not exported
+func newPiece(deltaX []int, deltaY []int) *Piece {
+	p := new(Piece)
+	p.deltaX = deltaX
+	p.deltaY = deltaY
+	return p
+}
 
-/* TODO: Methods for game
+//Constast type for direction
+type Direction int
 
-deletePiece()
+const (
+	down Direction = iota
+	left
+	right
+)
 
-placePiece()
 
-pieceFits()
+// Struct to model the game
+type Game struct {
+	x int
+	y int
+	//board[y][x]
+	board [][]int
+	piece Piece
+	clock *time.Timer
+}
 
-movePiece(direction=[down, left, right])
+// helper functions
+func initializeBoard(initValue int) [][]int {
+	b = make([][]int, boardHeight)
+	for y := 0; y < boardHeight; y++ {
+		b[y] make([]int, boardWidth)
+		for x := 0; x < boardWidth; x++ {
+			b[y][x] = initValue
+		}
+	}
+	return b
+}
 
-rotate()
+// TODO: Methods for game
 
-checkRow()
+func NewGame() (*Game) {
+	g := new(Game)
+	g.x = 1
+	g.y = 1
+	g.board = initializeBoard(0)
+	g.clock = time.NewTimer(gameSpeed)
 
-fillBoard()
+}
+func (g *Game) play() {
+	if movePiece(down) {
+		g.resetClock()
+	}
+}
 
-spawnPiece()
+func (g *Game) resetClock() {
+	g.clock.Reset(gameSpeed)
+}
+
+// deletePiece()
+
+// placePiece()
+
+// pieceFits()
+
+// movePiece(direction=[down, left, right])
+
+//rotate()
+
+//checkRow()
+
+//fillBoard()
+
+//spawnPiece()
 
 
 
